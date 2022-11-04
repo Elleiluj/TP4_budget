661a16f43af309ff8f5c66479ce153184c3dab78
Julie <julie.labbe@polymtl.ca>
2022-11-04 16:53:44 -0400

    Modification de l'appel du workflow

--- myscript.py
+++ myscript.py
@@ -1,8 +1,12 @@
 import os
+import sys
 
 
 def main():
-    os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
+    # Cherche les paramètres (hash des commits) dans l'appel du script
+    args = sys.argv[1:]
+
+    os.system('git bisect start %s %s' % (args[0], args[1]))
     os.system('git bisect run python manage.py test')
     os.system('git bisect reset')
 
